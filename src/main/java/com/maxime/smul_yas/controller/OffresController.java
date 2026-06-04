c212b9f0b9e8ccc985a06063cda7bb4d2c98cd6c
maxUtilisateur <m45990635@gmail.com>
2026-06-04 10:18:40 +0000

    premier

--- src/main/java/com/maxime/smul_yas/controller/OffresController.java
+++ src/main/java/com/maxime/smul_yas/controller/OffresController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/offres")
 @RequiredArgsConstructor
-public class OffresController {
+public class  OffresController {
 
     private final OffresService offresService;
 
